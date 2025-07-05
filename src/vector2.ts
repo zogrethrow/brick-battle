a84edb5862dacd428b66cf527fd6f6746d97576a
sukus <52664542+sukus21@users.noreply.github.com>
2025-07-05 23:43:07 +0200

    add Vector2 math methods

--- src/vector2.ts
+++ src/vector2.ts
@@ -4,4 +4,40 @@ export default class Vector2 {
         public y: number,
     ) {
     }
+
+    /**
+     * Get the length of the vector
+     */
+    get length(): number {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    directonTo(other: Vector2): number {
+        return Math.atan2(
+            other.x - this.x,
+            other.y - this.y,
+        );
+    }
+
+    add(other: Vector2): Vector2 {
+        return new Vector2(
+            this.x + other.x,
+            this.y + other.y,
+        );
+    }
+
+    sub(other: Vector2): Vector2 {
+        return new Vector2(
+            this.x - other.x,
+            this.y - other.y,
+        );
+    }
+
+    normalize(): Vector2 {
+        const length = this.length;
+        return new Vector2(
+            this.x / length,
+            this.y / length,
+        );
+    }
 }
